55529d754ccf2fb19b85402d0f0b470fe4aa01e6
Danny <danny@macbook-pro-de-elinda.local>
2018-10-18 17:38:29 +0200

    Initial Commit

---
+++ starexplorer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starexplorer
+//
+//  Created by Danny on 18.10.18.
+//  Copyright © 2018 AFDanny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ starexplorerTests/starexplorerTests.swift
@@ -0,0 +1,34 @@
+//
+//  starexplorerTests.swift
+//  starexplorerTests
+//
+//  Created by Danny on 18.10.18.
+//  Copyright © 2018 AFDanny. All rights reserved.
+//
+
+import XCTest
+@testable import starexplorer
+
+class starexplorerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
